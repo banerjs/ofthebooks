ade921b8e31cfda23a4594908f92179fb4b9267f
Siddhartha Banerjee <banerjs.sid@gmail.com>
2015-08-07 16:23:18 +0530

    Trying to fix heroku

--- ofthebooks/settings/prod.py
+++ ofthebooks/settings/prod.py
@@ -7,7 +7,9 @@ from .common import *
 
 ALLOWED_HOSTS = ['*']
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {
+	'default': dj_database_url.config()
+}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
